b800027e4c27747bbba4fc592bd5c200e466d385
SangeethaShanmugm <83452706+SangeethaShanmugm@users.noreply.github.com>
2021-06-09 11:02:21 +0530

    Add files via upload

---
+++ callbackexample.js
@@ -0,0 +1,47 @@
+// setTimeout(()=>{    console.log("10");}, 1000);
+// setTimeout(()=>{    console.log("9");}, 1000);
+// setTimeout(()=>{    console.log("8");}, 1000);
+// setTimeout(()=>{    console.log("10");}, 1000);
+
+
+
+
+
+setTimeout(() => {
+    console.log("10");
+    setTimeout(() => {
+        console.log("9");
+       setTimeout(() => {
+            console.log("8");
+            setTimeout(() => {
+                console.log("7");
+                setTimeout(() => {
+                    console.log("6");
+                    setTimeout(() => {
+                        console.log("5");
+                        setTimeout(() => {
+                            console.log("4");
+                            setTimeout(() => {
+                                console.log("3");
+                                setTimeout(() => {
+                                    console.log("2");
+                                    setTimeout(() => {
+                                        console.log("1");
+                                           setTimeout(() => {
+                                              console.log("Happy Independance Day ");
+            },1000);
+           },1000);
+       },1000);
+    },1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+
+
+
+    
+    
